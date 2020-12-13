a0a30a85826adfefe6beb32d9fda67b979722e60
EmeraIdDeveloper <75602136+EmeraIdDeveloper@users.noreply.github.com>
2020-12-13 12:16:43 -0700

    Create Tnp-Tags-List.lua

---
+++ Tnp-Tags-List.lua
@@ -0,0 +1,33 @@
+local list = {
+	--Owners
+	["189975315"] = "Owner"; --WhakeNeD
+	--Co-Owners
+	--Devs
+	["407480830"] = "Developer"; --TheEmeraIdDev
+	["596469404"] = "Developer"; --Kachisune
+	["59119778"] = "Developer"; --Saf_fyre
+	--Admins
+	--Mods
+	["1415880068"] = "Mod"; --TeasTown
+	["81404095"] = "Mod"; --bestgamer1223
+	--Helpers
+	["97679780"] = "Helper"; --Mahdio16
+	["135945729"] = "Helper"; --ArtisticPurpleGuy
+	["348627227"] = "Helper"; --inaneseven
+	["185622553"] = "Helper"; --Smartdude32
+	["104768540"] = "Helper"; --ripElZ666
+	["35910456"] = "Helper"; --Sabatonic
+	--Curators
+	["1561894225"] = "Curator"; --theabcsAAERTy
+	["98156531"] = "Curator"; --JustAnotherGaming
+	["1397380556"] = "Curator"; --helloitsmelol23
+	--Verified Builders
+	["1409670704"] = "Verified Builder"; --SwiftBahamut
+	["300114163"] = "Verified Builder"; --cesargjr
+	["309493948"] = "Verified Builder"; --BosscraftdanGamer
+	--Contributors
+	["60298492"] = "Contributor"; --Glisterr
+	["275053149"] = "Contributor"; --sokol50
+	["90784125"] = "Contributor"; --TheBirbsWord
+	["280178713"] = "Contributor"; --megamanyoutuber
+}
